eafa7ed22910d128ca0da3b4fe46386fd41d44bd
patrickijieh <ijiehpatrick@gmail.com>
2024-10-30 19:19:01 -0400

    Initial commit; creating separate branch for file rankings

--- bug_localization/bug_localization.py
+++ bug_localization/bug_localization.py
@@ -11,7 +11,14 @@ class BugLocalization:
 
     # Encoding Embeddings
     def encode_text(self, text):
-        """Encodes a single text (bug report or function) into an embedding."""
+        """
+        Encodes a single text (bug report, function, or body of code) into an embedding.
+        Args:
+            text: The body of text.
+
+        Returns:
+            embedding: The embedding for the body of text.
+        """
         tokens_ids = self.model.tokenize([text], max_length=512, mode="<encoder-only>")
         source_ids = torch.tensor(tokens_ids).to(self.device)
         _, embedding = self.model(source_ids)
@@ -19,7 +26,15 @@ class BugLocalization:
         return norm_embedding
 
     def encode_batch(self, texts):
-        """Encodes a batch of texts into embeddings."""
+        """
+        Encodes a batch of texts into embeddings.
+        
+        Args:
+            texts: The text batch.
+
+        Returns:
+            embeddings: The embeddings for the batch of texts.
+        """
         embeddings = []
         for text in texts:
             embeddings.append(self.encode_text(text))
@@ -30,22 +45,22 @@ class BugLocalization:
         """
         Ranks files based on similarity to the query embedding.
         
-        Parameters:
-        - query_embedding: The embedding of the bug report or query.
-        - db_embeddings: A list of tuples, where each tuple contains (file_id, embedding)
+        Args:
+            query_embedding: The embedding of the bug report or query.
+            db_embeddings: A list of tuples, where each tuple contains (file_id, embedding)
                          representing the file's unique identifier and its precomputed embedding.
                          
         Returns:
-        - A sorted list of (file_id, similarity_score) tuples in descending order of similarity.
+            similarities: A sorted list of (file_id, similarity_score) tuples in descending order of similarity.
         """
         # Normalize the query embedding
-        norm_query_embedding = torch.nn.functional.normalize(query_embedding, p=2, dim=1)
+        # norm_query_embedding = torch.nn.functional.normalize(query_embedding, p=2, dim=1)
         
         # Calculate similarity scores
         similarities = []
         for file_id, embedding in db_embeddings:
             db_embedding = torch.tensor(embedding).to(self.device)
-            similarity = torch.einsum("ac,bc->ab", norm_query_embedding, db_embedding).item()
+            similarity = torch.einsum("ac,bc->ab", db_embedding, query_embedding).item()
             similarities.append((file_id, similarity))
         
         # Sort files by similarity score in descending order
