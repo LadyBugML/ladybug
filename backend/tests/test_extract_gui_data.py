ba3aea667b6e92a017618f53fa73b25499cc0d35
Darren Bansil <darrenbansil@gmail.com>
2025-01-25 13:11:19 -0500

    Add test case for building corpus

--- backend/tests/test_extract_gui_data.py
+++ backend/tests/test_extract_gui_data.py
@@ -1,5 +1,6 @@
 from services.extract_gui_data import extract_sc_terms
 from services.extract_gui_data import extract_gs_terms
+from services.extract_gui_data import build_corpus
 
 def test_extract_SC_terms():
     json_path = "temp_testing/Execution-1.json"
@@ -49,3 +50,29 @@ def test_extract_GS_terms():
     extracted_terms = sorted(extract_gs_terms(json_path))
 
     assert extracted_terms == expected_gs_terms, f"Mismatch: {extracted_terms}"
+
+def test_build_corpus():
+    sc_terms = [
+        'add_expense',
+        'content',
+        'select_account',
+        'toolbar'
+    ]
+
+    source_code_files = [
+        ('path/to/Expenses.java', 'Expenses.java', 'public static void main(String[] args) { int add_expense = 5;}'),
+        ('path/to/file1', 'file1', 'mock code'),
+        ('path/to/file2', 'file1', 'mock code'),
+        ('path/to/ToolbarScreen.java', 'ToolbarScreen.java', 'public static void main(String[] args) { int toolbar = 5;}'),
+        ('path/to/file3', 'file1', 'mock code'),
+        ('path/to/file1', 'file1', 'mock code'),
+    ]
+
+    expected_corpus_files = [
+        'path/to/Expenses.java',
+        'path/to/ToolbarScreen.java'
+    ]
+
+    corpus_files = build_corpus(source_code_files, sc_terms)
+
+    assert corpus_files == expected_corpus_files, f"Mismatch: {corpus_files}"
